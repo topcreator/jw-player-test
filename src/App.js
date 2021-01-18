ad5a0eb1e217523cdc8108e50485e429424126eb
topcreator <kopylov.roman1988@gmail.com>
2021-01-18 15:43:17 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -6,20 +6,19 @@ import './App.css';
 const playlist = [{
   default: true,
   endtime: 0,
-  file: "https://www.dropbox.com/s/iy4gwh9t3efm0uh/MIN_05907_HD%20-%20Moxy%20Fruvous%20Interview.mp4?raw=1",
-  // file: 'https://www.youtube.com/watch?v=go_nJ42a8PE',
+  file: "https://content.jwplatform.com/manifests/yp34SRmf.m3u8",
   starttime: 0,
   status: true,
-  title: "IDLES - Full Performance (Live on KEXP)",
+  title: "Test video - 1",
 },
 {
   default: false,
   endtime: 0,
-  file: "https://www.dropbox.com/s/iy4gwh9t3efm0uh/MIN_05907_HD%20-%20Moxy%20Fruvous%20Interview.mp4?raw=1",
+  file: "//videos.electroteque.org/bitrate/big_buck_bunny_600k.mp4",
   // file: 'https://www.youtube.com/watch?v=NztNVRQ4E1Y',
   starttime: 0,
   status: true,
-  title: "IDLES - Full Performance (Live on KEXP)----2",
+  title: "Test Video ----2",
 },
 {
   default: false,
@@ -28,13 +27,15 @@ const playlist = [{
   // file: 'https://www.youtube.com/watch?v=0lbH_Z-Xi1Q',
   starttime: 0,
   status: true,
-  title: "IDLES - Full Performance (Live on KEXP)-----3",
+  title: "Test Video -----3",
 }];
-function App() {
 
+let player;
+function App() {
+  const [title, setTitle] = React.useState('');
   const onReady = () => {
     console.log('onReady')
-    const player = window.jwplayer('jw-player');
+    // const player = window.jwplayer('jw-player');
     console.log(player.getPlaylist());
     // player.on('markersupdated', data => {
     //   if (player && typeof player.getMarkersVTT === 'function') {
@@ -51,10 +52,11 @@ function App() {
 
   const onTime = event => {
     // console.log('onTime===>', event);
-    const player = window.jwplayer('jw-player');
+    // const player = window.jwplayer('jw-player');
     const playlistCurrentItem = player.getPlaylistIndex();
     const video = player.getPlaylistItem(playlistCurrentItem);
     console.log('onTime==>', playlistCurrentItem, video);
+    setTitle(video.title);
     // const endtime = playlist[0].endtime;
     // const starttime = playlist[0].starttime;
     // if(player) {
@@ -95,18 +97,18 @@ function App() {
   //   }
   // }
 
-  const onUpdateMarkers = () => {
-    const player = window.jwplayer('jw-player');
-    if (player && typeof player.setMarkers === 'function') {
-      const markers = [
-        { startTime: 100, endTime: 600, text: "Marker 1", color: 'blue'},
-        { startTime: 650, endTime: 900, text: "Marker 2", color: 'green' },
-        { startTime: 950, endTime: 1800, text: "Marker 3", color: 'red' }
-      ];
-      console.log('update markers==>', markers);
-      player.setMarkers(markers);
-    }
-  }
+  // const onUpdateMarkers = () => {
+  //   const player = window.jwplayer('jw-player');
+  //   if (player && typeof player.setMarkers === 'function') {
+  //     const markers = [
+  //       { startTime: 100, endTime: 600, text: "Marker 1", color: 'blue'},
+  //       { startTime: 650, endTime: 900, text: "Marker 2", color: 'green' },
+  //       { startTime: 950, endTime: 1800, text: "Marker 3", color: 'red' }
+  //     ];
+  //     console.log('update markers==>', markers);
+  //     player.setMarkers(markers);
+  //   }
+  // }
 
   // const plugins = {
   //   'js/videomarkers-8.1.0.js': {
@@ -114,6 +116,10 @@ function App() {
   //     navigation: true,
   //   },
   // };
+
+  React.useEffect(() => {
+    player = window.jwplayer('jw-player');
+  }, []);
 
   return (
     <div className="App">
@@ -130,7 +136,11 @@ function App() {
           onTime={onTime}
         />
       </div>
-      <button onClick={onUpdateMarkers}>update markers</button>
+      {/* <button onClick={onUpdateMarkers}>update markers</button> */}
+      <div style={{marginTop: 30, textAlign: "left"}}>
+        <strong>Title: </strong>
+        <span>{title}</span>
+      </div>
     </div>
   );
 }
