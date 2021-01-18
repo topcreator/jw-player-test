c74ae894c5155cac52213910136a7656974cd268
Admin <admin@Admins-iMac-5.local>
2021-01-18 04:44:38 +0200

    init project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,136 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactJWPlayer from 'react-jw-player';
 import './App.css';
 
+// let jwInterval = null;
+const playlist = [{
+  default: true,
+  endtime: 0,
+  file: "https://www.dropbox.com/s/iy4gwh9t3efm0uh/MIN_05907_HD%20-%20Moxy%20Fruvous%20Interview.mp4?raw=1",
+  // file: 'https://www.youtube.com/watch?v=go_nJ42a8PE',
+  starttime: 0,
+  status: true,
+  title: "IDLES - Full Performance (Live on KEXP)",
+},
+{
+  default: false,
+  endtime: 0,
+  file: "https://www.dropbox.com/s/iy4gwh9t3efm0uh/MIN_05907_HD%20-%20Moxy%20Fruvous%20Interview.mp4?raw=1",
+  // file: 'https://www.youtube.com/watch?v=NztNVRQ4E1Y',
+  starttime: 0,
+  status: true,
+  title: "IDLES - Full Performance (Live on KEXP)----2",
+},
+{
+  default: false,
+  endtime: 0,
+  file: "https://www.dropbox.com/s/iy4gwh9t3efm0uh/MIN_05907_HD%20-%20Moxy%20Fruvous%20Interview.mp4?raw=1",
+  // file: 'https://www.youtube.com/watch?v=0lbH_Z-Xi1Q',
+  starttime: 0,
+  status: true,
+  title: "IDLES - Full Performance (Live on KEXP)-----3",
+}];
 function App() {
+
+  const onReady = () => {
+    console.log('onReady')
+    const player = window.jwplayer('jw-player');
+    console.log(player.getPlaylist());
+    // player.on('markersupdated', data => {
+    //   if (player && typeof player.getMarkersVTT === 'function') {
+    //     console.log(data.markers);
+    //   }
+    // });
+    player.on('beforeComplete', () => {
+      console.log('beforeComplete===>', player.getPlaylistIndex(), player.getPlaylist());
+      // player.next();
+      // document.querySelector('iframe.jw-video').style.display = 'none';
+      // document.querySelector('.jw-preview').style.display = 'block';
+    });
+  };
+
+  const onTime = event => {
+    // console.log('onTime===>', event);
+    const player = window.jwplayer('jw-player');
+    const playlistCurrentItem = player.getPlaylistIndex();
+    const video = player.getPlaylistItem(playlistCurrentItem);
+    console.log('onTime==>', playlistCurrentItem, video);
+    // const endtime = playlist[0].endtime;
+    // const starttime = playlist[0].starttime;
+    // if(player) {
+    //   if (endtime > 0) {
+    //     if (event.position >= endtime) {
+    //       player.seek(endtime);
+    //       player.pause();
+    //     }
+    //   }
+
+    //   if (event.position < starttime) {
+    //     player.seek(starttime);
+    //   }
+    // }
+      
+  }
+
+  const onVideoLoad = event => {
+    console.log('onVideoLoad', event);
+    // jwInterval = setInterval(() => onShowMarkers(), 500);
+  }
+
+  // const onShowMarkers = () => {
+  //   console.log('called showMarkers==>');
+  //   const player = window.jwplayer('jw-player');
+  //   if (player && typeof player.setMarkers === 'function' && player.getDuration() > 0) {
+  //     console.log('players===>', player);
+  //     clearInterval(jwInterval);
+  //     const markers = [
+  //       {
+  //         startTime: 980,
+  //         endTime: 1330,
+  //         text: "title",
+  //         color: '#0088FE',
+  //       }
+  //     ];
+  //     player.setMarkers(markers);
+  //   }
+  // }
+
+  const onUpdateMarkers = () => {
+    const player = window.jwplayer('jw-player');
+    if (player && typeof player.setMarkers === 'function') {
+      const markers = [
+        { startTime: 100, endTime: 600, text: "Marker 1", color: 'blue'},
+        { startTime: 650, endTime: 900, text: "Marker 2", color: 'green' },
+        { startTime: 950, endTime: 1800, text: "Marker 3", color: 'red' }
+      ];
+      console.log('update markers==>', markers);
+      player.setMarkers(markers);
+    }
+  }
+
+  const plugins = {
+    '/js/videomarkers-8.1.0.js': {
+      editor: true,
+      navigation: true,
+    },
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{width: '100%'}}>
+        <ReactJWPlayer
+          className="clip-jw-player"
+          playerId="jw-player"
+          playerScript="https://cdn.jwplayer.com/libraries/L2lWZS0f.js"
+          playlist={playlist}
+          customProps={{ plugins }}
+          isAutoPlay
+          onReady={onReady}
+          onVideoLoad={onVideoLoad}
+          onTime={onTime}
+        />
+      </div>
+      <button onClick={onUpdateMarkers}>update markers</button>
     </div>
   );
 }
